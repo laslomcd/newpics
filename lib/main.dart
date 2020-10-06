b5ec85b55bb9692081e771595f201673b28b0f6e
ryan <laslomcd@gmail.com>
2020-10-06 17:18:09 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+void main() {
+  var app = MaterialApp(
+    home: Scaffold(
+      floatingActionButton: FloatingActionButton(),
+      appBar: AppBar(
+        title: Text('Let\'s See Some Images!'),
+      ),
+    ),
+  );
+
+  runApp(app);
+}
