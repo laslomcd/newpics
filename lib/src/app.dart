8f4a2b64eeff7fe67f8be99ea40cea3deed27762
ryan <laslomcd@gmail.com>
2020-10-08 15:54:49 -0400

    added photos API

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,10 @@
+import "dart:convert";
+
 import "package:flutter/material.dart";
+import 'package:http/http.dart' show get;
+
+import 'models/image_model.dart';
+import 'widgets/image_list.dart';
 
 class App extends StatefulWidget {
   createState() {
@@ -8,18 +14,25 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    final response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    final imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
+  }
 
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text('$counter'),
+        body: ImageList(images),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          },
+          onPressed: fetchImage,
         ),
         appBar: AppBar(
           title: Text('Let\'s See Some Images!'),
